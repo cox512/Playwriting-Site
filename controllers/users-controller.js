fa562a8c1cf8abea025a25d7a5e1e7f3e0bb4509
cox512 <64932698+cox512@users.noreply.github.com>
2020-07-08 16:23:37 -0500

    Added password encryption. New users can create username and a persistent, encrypted password.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const userRouter = express.Router();
+const bcrypt = require('bcrypt');
+
+const User = require('../models/users.js');
 
 userRouter.get('/new', (req, res) => {
     res.render('users/new.ejs');
 })
 
-
+userRouter.post('/', (req, res) => {
+    //Salt password to anonymize
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(9));
+    User.create(req.body, (err, createdUser) => {
+        console.log('new user:', createdUser);
+        res.redirect('/plays');
+    })
+})
 
 module.exports = userRouter;
