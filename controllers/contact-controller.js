1f7fc1b01a5a1d3d3bd99f523e7b80c29d299c0f
cox512 <64932698+cox512@users.noreply.github.com>
2020-11-11 21:47:51 -0600

    Set up nodemailer emailing in contact form

---
+++ controllers/contact-controller.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+
+//ROUTES
+router.get("/", (req, res) => {
+  res.render("contact.ejs", {
+    currentUser: req.session.currentUser,
+    titleBar: "Contact",
+  });
+});
+
+router.post("/", (req, res) => {
+  let transporter = nodemailer.createTransport({
+    host: "smtpout.secureserver.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: USERNAME,
+      pass: PASSWORD,
+    },
+  });
+
+  const mailOpts = {
+    from: `${req.body.name} ${req.body.email}`,
+    to: USERNAME,
+    subject: "New message from Playwriting contact form",
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`,
+  };
+
+  transporter.sendMail(mailOpts, (error, response) => {
+    if (error) {
+      res.render("failure.ejs", {
+        currentUser: req.session.currentUser,
+        titleBar: "Contact",
+      });
+    } else {
+      res.render("success.ejs", {
+        currentUser: req.session.currentUser,
+        titleBar: "Contact",
+      });
+    }
+  });
+});
+
+module.exports = router;
--- controllers/sessions-controller.js
+++ controllers/sessions-controller.js
@@ -1,44 +1,40 @@
-const express = require('express');
-const User = require('../models/users');
+const express = require("express");
+const User = require("../models/users");
 const sessionsRouter = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-sessionsRouter.get('/new', (req, res) => {
-    res.render('sessions/new.ejs', {
+sessionsRouter.get("/new", (req, res) => {
+  res.render("sessions/new.ejs", {
     currentUser: req.session.currentUser,
-    titleBar: 'Log-In'
-    });
-})
+    titleBar: "Log-In",
+  });
+});
 
-sessionsRouter.post('/', (req, res) => {
-    //Check to see if the user exists
-    User.findOne( { username: req.body.username }, (err, foundUser) => {
-        //Check for an error in the query
-        if (err) {
-            console.log(err);
-            res.send('Something bad happened in the database');
-        } else if (!foundUser) {
-            //if user isn'found in db. Re-direct and message.
-            res.send("<a href='/plays/'>Sorry, user not found.</a>")
-        } else {
-            //User exists and the passwords can be compared
-            if (bcrypt.compareSync(req.body.password, foundUser.password)) {
-                //If passwords match
-                req.session.currentUser =foundUser;
-                res.redirect('/plays');
-            } else {
-                //Let them know if the password is incorrect
-                res.send('<a href="/plays">Incorrect password.</a>')
-            }
-        }
-    })
-})
+sessionsRouter.post("/", (req, res) => {
+  //Check to see if the user exists
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    //Check for an error in the query
+    if (err) {
+      console.log(err);
+      res.send("Something bad happened in the database");
+    } else if (!foundUser) {
+      res.send("<a href='/'>Sorry, user not found.</a>");
+    } else {
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        req.session.currentUser = foundUser;
+        res.redirect("/plays");
+      } else {
+        res.send('<a href="/">Incorrect password.</a>');
+      }
+    }
+  });
+});
 
 //DELETE route for logging out
-sessionsRouter.delete('/', (req, res) => {
-    req.session.destroy(() => {
-        res.redirect('/plays');
-    })
-})
+sessionsRouter.delete("/", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/plays");
+  });
+});
 
-module.exports = sessionsRouter
+module.exports = sessionsRouter;
