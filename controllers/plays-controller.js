a4459450f18e91f8db6b831e6bb0983c84626b26
cox512 <64932698+cox512@users.noreply.github.com>
2020-11-18 14:43:28 -0600

    made index page responsive

--- controllers/plays-controller.js
+++ controllers/plays-controller.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
     cb(null, "./public/images");
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname + "-" + Date.now());
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
