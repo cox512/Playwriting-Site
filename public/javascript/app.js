a761a27d4e52de70fbc2372f8850b33e6f8eaff8
cox512 <64932698+cox512@users.noreply.github.com>
2020-11-20 21:12:15 -0600

    Added remove buttons to input fields in edit form

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,14 +1,27 @@
 const addField = (field) => {
-  event.preventDefault()
-    if (!$(`#${field}-info input:last`).val()) {
-      alert("Your input field is empty.");
-    } else {
-      document
-        .getElementById(`${field}-info`)
-        .insertAdjacentHTML(
-          "beforeend",
-          `<input type="text" name="${field}" class="form-control ${field}-array field-box">`
-        );
-    }
-  };
+  event.preventDefault();
+  if (!$(`#${field}-info input:last`).val()) {
+    alert("Your input field is empty.");
+  } else {
+    document
+      .getElementById(`${field}-info`)
+      .insertAdjacentHTML(
+        "beforeend",
+        `<input type="text" name="${field}" class="form-control ${field}-array field-box">`
+      );
+  }
+};
 
+const removeField = (evt, field) => {
+  evt.preventDefault();
+  let input = Array.from(document.getElementsByClassName(`${field}-array`));
+  console.log("input:", input.length);
+
+  for (let i = 0; i < input.length; i++) {
+    (function (index) {
+      input[i].onclick = function () {
+        input[index].remove();
+      };
+    })(i);
+  }
+};
