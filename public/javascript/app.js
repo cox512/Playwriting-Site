ad7a4eef91b6d54df30f14b84cb3705c0a1b9a33
Jacob Cox <jacob@championofvictory.com>
2021-01-02 16:01:02 -0600

    Re-classified index page

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,19 +1,25 @@
 //=================
 // Add a a new input field to the development and honors fields
 //=================
+
 const addField = (field) => {
   event.preventDefault();
-  if (!$(`#${field}-info input:last`).val()) {
+
+  let list = document.getElementById(`${field}-info`).childNodes;
+
+  if (!$(`#${field}-info input:last`).val() && list.length > 2) {
     alert("Your input field is empty.");
   } else {
     document.getElementById(`${field}-info`).insertAdjacentHTML(
       "beforeend",
       `<div class="input-group field-box">
-        <input type="text" name="${field}" class="form-control ${field}-array field-box">
+        <input type="text" name="${field}" id="${field}" class="form-control">
           <div class="input-group-append">
-            <span class="form__button--remove input-group-text remove-span" >
+            <span class="form__button--remove input-group-text" >
               <button class="close remove-button" onClick="removeField(event,'${field}')">
-              <span aria-hidden="true">&times;</span>
+                <span aria-hidden="true">
+                  &times;
+                </span>
               </button>
             </span>
           </div>    
@@ -27,7 +33,6 @@ const addField = (field) => {
 //=================
 const removeField = (evt, field) => {
   evt.preventDefault();
-  console.log("currentTarget.offsetParent", evt.currentTarget.offsetParent);
-  console.log("event.currentTarget", evt.currentTarget);
+
   evt.currentTarget.offsetParent.remove();
 };
